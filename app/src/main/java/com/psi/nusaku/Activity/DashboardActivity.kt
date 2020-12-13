71ed462e61af2ea9129f7768d937c4f2cc46e55f
Akbar Satya Nugraha <personal.akbarsn@gmail.com>
2020-12-13 18:07:10 +0700

    Fixing app bar

--- app/src/main/java/com/psi/nusaku/Activity/DashboardActivity.kt
+++ app/src/main/java/com/psi/nusaku/Activity/DashboardActivity.kt
@@ -14,10 +14,9 @@ class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        supportActionBar?.hide()
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
