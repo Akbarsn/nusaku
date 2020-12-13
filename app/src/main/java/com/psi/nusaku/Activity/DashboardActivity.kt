a83baeab232e4e08c2553194bb873b12bff4aa3b
Akbar Satya Nugraha <personal.akbarsn@gmail.com>
2020-12-13 18:03:57 +0700

    Binding Profile

--- app/src/main/java/com/psi/nusaku/Activity/DashboardActivity.kt
+++ app/src/main/java/com/psi/nusaku/Activity/DashboardActivity.kt
@@ -15,7 +15,6 @@ class DashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        supportActionBar?.hide()
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
--- app/src/main/java/com/psi/nusaku/Activity/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/psi/nusaku/Activity/ui/profile/ProfileFragment.kt
@@ -4,16 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.psi.nusaku.R
 
+
 class ProfileFragment : Fragment() {
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_profile, container, false)
+        val root = inflater.inflate(R.layout.layout_profile, container, false)
+        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
         return root
     }
 }
