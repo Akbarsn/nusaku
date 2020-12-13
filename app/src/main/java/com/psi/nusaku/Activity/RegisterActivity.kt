52b4a1921fd189a09e255c65e35285dd248bc26e
Akbar Satya Nugraha <personal.akbarsn@gmail.com>
2020-12-13 11:52:19 +0700

    Fixing Register

--- app/src/main/java/com/psi/nusaku/Activity/RegisterActivity.kt
+++ app/src/main/java/com/psi/nusaku/Activity/RegisterActivity.kt
@@ -24,6 +24,7 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        supportActionBar?.hide()
 
         etEmail = findViewById(R.id.et_RinputEmail)
         etFullName = findViewById(R.id.et_RinputFullName)
